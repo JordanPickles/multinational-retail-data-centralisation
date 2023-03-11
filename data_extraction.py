c61dc3733070c2b7ff30459a1439b1b3ae72f5ad
JordanPickles <jordan.l.pickles@gmail.com>
2023-03-11 11:11:11 +0000

    adds database connection

--- data_extraction.py
+++ data_extraction.py
@@ -1,15 +1,28 @@
 import pandas as pd
 import numpy as np
 from database_utils import DatabaseConnector
-
+from sqlalchemy import create_engine, inspect
+import psycopg2
 
 class DataExtractor:
     def __init__(self):
         pass
 
-    def read_rds_table(self):
-        pass # it will take in an instance of the database connector class and the table name as an argument and return a pd df
+
+    def read_rds_table(self, table_name):
+        connector = DatabaseConnector()
+        engine = connector.init_db_engine(connector.read_db_creds())
+        tables = connector.list_db_tables(engine)
+        engine = engine.connect()
+
+        user_data = pd.read_sql_table(table_name, engine)
+        return user_data # it will take in an instance of the database connector class and the table name as an argument and return a pd df
         # Use list_db_tables method to get the name of the table containing user data
         # Use read_rds_table method to extract the table containing user data and return a pd df
         
 
+
+
+instance = DataExtractor()
+legacy_users_table = instance.read_rds_table('legacy_users')
+legacy_users_table.to_csv('.legacy_users.csv')
--- database_utils.py
+++ database_utils.py
@@ -1,19 +1,33 @@
 import pandas as pd
 import numpy as np
 import yaml
+from sqlalchemy import create_engine, inspect
+import psycopg2
+import os
+
 
 class DatabaseConnector:
     def __init__(self):
         pass
 
     def read_db_creds(self):
-        pass #Take in yaml and return a dict
+        with open("db_creds.yaml", "r") as f:
+            db_creds = yaml.safe_load(f)
+        return db_creds #Take in yaml and return a dict
 
-    def init_db_engine(self):
-        pass # Take in the db_creds output and initialise and return an sql_alchemy database engine
+    def init_db_engine(self, db_creds):
+        engine = create_engine(f"{db_creds['RDS_DATABASE_TYPE']}+{db_creds['DB_API']}://{db_creds['RDS_USER']}:{db_creds['RDS_PASSWORD']}@{db_creds['RDS_HOST']}:{db_creds['RDS_PORT']}/{db_creds['RDS_DATABASE']}")
+        engine.connect()
+        
+        # Take in the db_creds output and initialise and return an sql_alchemy database engine
+        return engine
 
-    def list_db_tables(self):
-        pass # Using the engine from init_db_engine() list all the tables in the database so you know whhich tables you can extract data from
+    def list_db_tables(self, engine):
+        engine.connect()
+        inspector = inspect(engine)
+        table_names = inspector.get_table_names()
+        return table_names
+        # Using the engine from init_db_engine() list all the tables in the database so you know whhich tables you can extract data from
 
     def upload_to_db(self):
         pass #this method will take in a pd df and a table name to upload to as an argument
@@ -23,3 +37,5 @@ class DatabaseConnector:
 
 
 
+instance = DatabaseConnector()
+instance.list_db_tables(instance.init_db_engine(instance.read_db_creds()))
