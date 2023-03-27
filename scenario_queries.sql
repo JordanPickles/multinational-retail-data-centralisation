0bd00b0186ae4f85e867cde4617e4bca8d1be71c
JordanPickles <jordan.l.pickles@gmail.com>
2023-03-27 21:31:23 +0100

    completes queries

--- scenario_queries.sql
+++ scenario_queries.sql
@@ -55,15 +55,16 @@ ORDER BY product_quantity_count;
 SELECT 
 	dim_store_details.store_type as store_details,
 	SUM(orders_table.product_quantity * dim_products.product_price) as number_of_sales,
+	
 	SUM(orders_table.product_quantity * dim_products.product_price)	/ 
 	(SELECT SUM(orders_table.product_quantity * dim_products.product_price) FROM orders_table
 	 	LEFT JOIN dim_products on orders_table.product_code = dim_products.product_code)*100 as total_percent
+
 FROM orders_table
 	LEFT JOIN dim_store_details on orders_table.store_code = dim_store_details.store_code
 	LEFT JOIN dim_products on orders_table.product_code = dim_products.product_code
 GROUP BY store_details
 ORDER BY number_of_sales desc;
-
 
 
 -- Which month in each year produced the highest cost of sales?
@@ -93,5 +94,44 @@ GROUP BY dim_store_details.store_type;
 -- How quickly is the company making sales?
 -- Sales would like to get an accurate metric for how quickly the compnay is making sales.
 -- Determine the average time take between each sale grouped by year.
+with time_table(hour, minutes, seconds, day, month, year, date_uuid) as (
+	SELECT 
+		EXTRACT(hour from CAST(timestamp as time)) as hour,
+		EXTRACT(minute from CAST(timestamp as time)) as minutes,
+		EXTRACT(second from CAST(timestamp as time)) as seconds,
+		day as day,
+		month as month,
+		year as year,
+		date_uuid
+	FROM dim_date_times),
+	
+	timestamp_table(timestamp, date_uuid, year) as (
+	SELECT MAKE_TIMESTAMP(CAST(time_table.year as int), CAST(time_table.month as int),
+						  CAST(time_table.day as int), CAST(time_table.hour as int),	
+						  CAST(time_table.minutes as int), CAST(time_table.seconds as float)) as order_timestamp,
+		time_table.date_uuid as date_uuid, 
+		time_table.year as year
+	FROM time_table),
+	
+	time_stamp_diffs(year, time_diff) as (
+	SELECT timestamp_table.year, timestamp_table.timestamp - LAG(timestamp_table.timestamp) OVER (ORDER BY timestamp_table.timestamp asc) as time_diff
+	FROM orders_table
+	JOIN timestamp_table ON orders_table.date_uuid = timestamp_table.date_uuid),
+
+	year_time_diffs(year, average_time_diff) as (
+		SELECT year, AVG(time_diff) as average_time_diff
+		FROM time_stamp_diffs
+		GROUP BY year
+		ORDER BY average_time_diff desc)
+		
+SELECT year, CONCAT('hours: ', EXTRACT(HOUR FROM average_time_diff),
+					'  minutes: ', EXTRACT(MINUTE FROM average_time_diff),
+				   '  seconds: ', CAST(EXTRACT(SECOND FROM average_time_diff) as int),
+				   '  milliseconds: ', CAST(EXTRACT(MILLISECOND FROM average_time_diff) as int))
+					
+FROM year_time_diffs;
+	
+
+
 
 	
